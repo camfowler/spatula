5cd45accf08a63027948e393a4ea50200164565d
Cameron Fowler <cameron.fowler@abletech.co.nz>
2017-03-24 18:19:51 +1300

    Use dotenv for creds thanks

--- lib/spatula.rb
+++ lib/spatula.rb
@@ -1,6 +1,13 @@
 require "spatula/version"
 require "spatula/rss_check"
 
+require 'dotenv'
+Dotenv.load
+
+raise ".env file missing" if ENV['PAPRIKA_TOKEN'].nil?
+
 module Spatula
+
+
   # Your code goes here...
 end
